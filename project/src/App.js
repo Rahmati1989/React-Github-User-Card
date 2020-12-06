1db3f0481d8a60b17ab21f5e47e7bce79b5c1e49
Rafiullah Rahmati <rafiullahrahmati@Rafiullahs-MacBook-Pro.local>
2020-12-05 21:52:49 -0800

    Finish The Project

--- project/src/App.js
+++ project/src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+ import axios from 'axios';
+ import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  class App extends React.Component{
+   constructor(){
+     super();
+     this.state={
+       user:{},
+       followers:[]
+     }
+   }
 
-export default App;
+    componentDidMount(){
+     axios
+       .get('https://api.github.com/users/Rahmati1989')
+       .then(res=>this.setState({user:res.data},console.log(res.data)))
+       .catch(err=>console.log(err));
+     axios
+       .get('https://api.github.com/users/Rahmati1989/followers')
+       .then(res=>this.setState({followers:res.data},console.log(res.data)))
+       .catch(err=>console.log(err));
+   }
+
+    render(){
+     return (
+       <div className="App">
+         <header className="App-header">
+         <h1 className='animation head'>  Rafiullah Git Hub</h1>
+         <div className='card'>
+           <img className='profilePic' src={this.state.user.avatar_url} alt={this.state.user.login}/>
+           <h3>{this.state.user.name}</h3>
+           <p>{this.state.user.login}</p>
+           <span>Profile: <a href={this.state.user.html_url}>{this.state.user.html_url}</a></span> 
+           <p>Followers: {this.state.user.followers}</p>
+           <p>Following: {this.state.user.following}</p>
+         </div>
+         <h2>Followers</h2>
+         {this.state.followers.map(follower=>
+         <div className='card' key={follower.id}>
+           <img className='profilePic' src={follower.avatar_url} alt={follower.login}/>
+           <h3>User:{follower.login}</h3>
+           <p>Profile:<a href={follower.html_url}>{follower.html_url}</a></p>
+         </div>
+       )}
+         </header>
+       </div>
+     );}
+   }
+
+ 
+  export default App;
